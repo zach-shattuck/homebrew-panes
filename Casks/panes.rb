bbe2d2c72b087672f45aaefdb3d26a68686a3573
Zach Shattuck <zachshattuck17@gmail.com>
2026-06-25 06:35:04 -0700

    Update Panes cask to 0.1.2

--- Casks/panes.rb
+++ Casks/panes.rb
@@ -1,6 +1,6 @@
 cask "panes" do
-  version "0.1.1"
-  sha256 "080220e766fcf1e8c0da6528a5bc6b37298ce64d90500c677a11d157a7a23a05"
+  version "0.1.2"
+  sha256 "e33c65a08b5c5c37cc5c2eb2e1ab3d866d6652666c8555c9c80b5c36000796b5"
 
   url "https://github.com/zach-shattuck/panes/releases/download/v#{version}/Panes.zip"
   name "Panes"
